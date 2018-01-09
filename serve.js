6ef8aa56422642991e836e7a52ddac68c8890e28
Lucas Del Pozzi <lucas@codetry.io>
2018-01-09 14:52:17 -0300

    Elimino vestigios de primeros intentos

--- serve.js
+++
@@ -1,5 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(2020, function(){
-    console.log('Server running on http://localhost:2020...');
-});
